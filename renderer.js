0e223124cc57eee029a066d89fa30a90bca542f2
Niangao-Warren <503809815@qq.com>
2020-06-16 01:07:33 +0800

    Delete renderer.js

--- renderer.js
+++
@@ -1,9 +0,0 @@
-// renderer.js
-const { ipcRenderer } = require('electron')
-
-window.addEventListener('DOMContentLoaded', () => {
-  ipcRenderer.send('message', 'hi from renderer')
-  ipcRenderer.on('reply', (event, arg) => {
-    document.getElementById('msg').innerHTML = arg
-  })
-})
